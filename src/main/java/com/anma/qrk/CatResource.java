2d8db1641e5c8e962ef4626e986af59a89c45ca9
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-03-12 13:00:57 +0200

    jdbc example with cloud db

--- src/main/java/com/anma/qrk/CatResource.java
+++ src/main/java/com/anma/qrk/CatResource.java
@@ -1,16 +1,24 @@
 package com.anma.qrk;
 
+import com.anma.qrk.model.Cat;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/cats")
 public class CatResource {
 
+    @Inject
+    EntityManager entityManager;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public List<Cat> cats() {
+        return entityManager.createNamedQuery("Cat.findAll", Cat.class).getResultList();
     }
 }
